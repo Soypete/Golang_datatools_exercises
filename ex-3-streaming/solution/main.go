73d06b3853b3d0be3459891bbb8665125fcf31d2
soypete <petersonmiriah@gmail.com>
2023-10-31 15:33:46 -0600

    ex-3-streaming: barebones solutions for aggregating streamed data
    Signed-off-by: soypete <petersonmiriah@gmail.com>

---
+++ ex-3-streaming/solution/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
+
+	"expvar"
+
+	"github.com/nats-io/nats.go"
+)
+
+type Message struct {
+	ID        int
+	Type      string
+	Body      UserAction
+	Timestamp time.Time
+}
+
+// action types: login, logout, purchase, return, add to cart, remove from cart, view item, view cart, view purchase history
+type UserAction struct {
+	UserID    int
+	Action    string
+	Succeded  bool
+	Error     string
+	Timestamp time.Time
+}
+
+var (
+	countLogin               expvar.Int
+	countLogout              expvar.Int
+	countPurchase            expvar.Int
+	countReturn              expvar.Int
+	countAddToCart           expvar.Int
+	countRemoveFromCart      expvar.Int
+	countViewItem            expvar.Int
+	countViewCart            expvar.Int
+	countViewPurchaseHistory expvar.Int
+)
+
+func main() {
+	// setup expvar server
+	expvar.Publish("countLogin", &countLogin)
+	expvar.Publish("countLogout", &countLogout)
+	expvar.Publish("countPurchase", &countPurchase)
+	expvar.Publish("countReturn", &countReturn)
+	expvar.Publish("countAddToCart", &countAddToCart)
+	expvar.Publish("countRemoveFromCart", &countRemoveFromCart)
+	expvar.Publish("countViewItem", &countViewItem)
+	expvar.Publish("countViewCart", &countViewCart)
+	expvar.Publish("countViewPurchaseHistory", &countViewPurchaseHistory)
+
+	// setup NATS
+	fmt.Println("Connecting to NATS...")
+	nc, err := nats.Connect("http://localhost:4222")
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(10 * time.Second)
+
+	nc.Subscribe("Actions", handleMessage)
+	nc.Flush()
+
+	if err := nc.LastError(); err != nil {
+		log.Fatal(err)
+	}
+
+	go http.ListenAndServe(":8080", nil)
+
+	runtime.Goexit()
+}
+
+func handleMessage(m *nats.Msg) {
+
+	var msg Message
+	err := json.Unmarshal(m.Data, &msg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	switch msg.Body.Action {
+	case "login":
+		countLogin.Add(1)
+		// handle login
+	case "logout":
+		countLogout.Add(1)
+		// handle logout
+	case "purchase":
+		countPurchase.Add(1)
+		// handle purchase
+	case "return":
+		countReturn.Add(1)
+		// handle return
+	case "add to cart":
+		countAddToCart.Add(1)
+		// handle add to cart
+	case "remove from cart":
+		countRemoveFromCart.Add(1)
+		// handle remove from cart
+	case "view item":
+		countViewItem.Add(1)
+		// handle view item
+	case "view cart":
+		countViewCart.Add(1)
+		// handle view cart
+	case "view purchase history":
+		countViewPurchaseHistory.Add(1)
+		// handle view purchase history
+	default:
+		fmt.Println("Unknown message type")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/Soypete/Golang_datatools_exercises
+
+go 1.21.0
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/nats-io/nats.go v1.31.0
+	github.com/pressly/goose/v3 v3.15.1
+	github.com/soypete/WebServices-in-3-weeks v0.0.0-20231015040516-f9017a42446c
+)
+
+require (
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/nats-io/nkeys v0.4.5 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
